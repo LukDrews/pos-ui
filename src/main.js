8f8e4472c60a23877d94e08585876bef33caec44
Lukas Drews <info@bytesandbits.net>
2022-01-06 14:50:57 +0100

    Add template files

---
+++ src/main.js
@@ -0,0 +1,19 @@
+import Vue from 'vue';
+import axios from 'axios';
+import VueAxios from 'vue-axios';
+import App from './App.vue';
+import router from './router';
+import store from './store';
+import vuetify from './plugins/vuetify';
+
+axios.defaults.baseURL = 'https://jsonplaceholder.typicode.com/';
+Vue.use(VueAxios, axios);
+
+Vue.config.productionTip = false;
+
+new Vue({
+  router,
+  store,
+  vuetify,
+  render: (h) => h(App),
+}).$mount('#app');
