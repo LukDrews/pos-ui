5d9acf76d7b82a099d6dcc0b1bcb0e6cfb02b6b6
Lukas Drews <info@bytesandbits.net>
2022-01-06 23:15:51 +0100

    Add navigation component

--- src/views/Navigation.vue
+++ src/views/Navigation.vue
@@ -1,15 +1,52 @@
 <template>
-    <div>
-        
-    </div>
+  <div>
+    <v-navigation-drawer
+      app
+      v-model="drawer"
+      :mini-variant="mini"
+      mini-variant-width="56"
+      permanent
+    >
+      <v-list-item class="px-2">
+        <v-btn icon @click.stop="mini = !mini">
+          <v-icon v-if="!mini">mdi-chevron-left</v-icon>
+          <v-icon v-if="mini">mdi-chevron-right</v-icon>
+        </v-btn>
+      </v-list-item>
+
+      <v-divider></v-divider>
+
+      <v-list dense>
+        <v-list-item class="ma-2" v-for="item in items" :key="item.title" :to="item.path">
+          <v-list-item-icon>
+            <v-icon>{{ item.icon }}</v-icon>
+          </v-list-item-icon>
+
+          <v-list-item-content>
+            <v-list-item-title>{{ item.title }}</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+
+    <v-app-bar app></v-app-bar>
+  </div>
 </template>
 
 <script>
 export default {
   name: 'Navigation',
+  data: () => ({
+    drawer: true,
+    items: [
+      { title: 'Home', icon: 'mdi-home-city', path: '/' },
+      { title: 'My Account', icon: 'mdi-account', path: '/about' },
+      { title: 'Users', icon: 'mdi-account-group-outline', path: '/users' },
+    ],
+    mini: false,
+  }),
 };
 </script>
 
 <style>
-
 </style>
