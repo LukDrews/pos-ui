d2ccabd99c26a025d1ebc0ac23c92fe88418e4a6
Lukas Drews <info@bytesandbits.net>
2022-01-27 13:06:35 +0100

    Add barcode validator

---
+++ src/validators/barcode.js
@@ -0,0 +1,59 @@
+/**
+ * @see https://www.gs1.org/services/how-calculate-check-digit-manually
+ * @author Lukas Drews <info@bytesandbits.net>
+ */
+
+const formats = {
+  ean8: { validChars: /^\d{8}$/, validLength: 8 },
+  ean12: { validChars: /^\d{12}$/, validLength: 12 },
+  ean13: { validChars: /^\d{13}$/, validLength: 13 },
+  ean14: { validChars: /^\d{14}$/, validLength: 14 },
+  ean18: { validChars: /^\d{18}$/, validLength: 18 },
+};
+
+function calculateChecksum(barcode) {
+  const arr = barcode
+    .substring(0, barcode.length - 1)
+    .split('')
+    .reverse();
+
+  const sum = arr
+    .map((d, i) => (i % 2 === 0 ? Number(d) * 3 : Number(d)))
+    .reduce((acc, d) => acc + d);
+
+  const checkSum = (10 - (sum % 10)) % 10;
+  return checkSum;
+}
+
+function validateFormat(barcode, format) {
+  return barcode.length === format.validLength && format.validChars.test(barcode);
+}
+
+function validateBarcode(barcode) {
+  const lastDigit = Number(barcode.substring(barcode.length - 1));
+  const checkSum = calculateChecksum(barcode);
+  return checkSum === lastDigit;
+}
+
+function validate(barcode) {
+  // check if format is found
+  const format = Object.keys(formats).find((f) => validateFormat(barcode, formats[f]));
+
+  return {
+    format,
+    valid: format ? validateBarcode(barcode) : false,
+  };
+}
+
+function isValidFormat(barcode, format) {
+  if (validateFormat(barcode, format)) {
+    return validateBarcode(barcode);
+  }
+  return false;
+}
+
+export default {
+  formats,
+  validate,
+  isValidFormat,
+};
