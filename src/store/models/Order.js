32fca8a6c5058207dbe7a9e3120b614e64e8b960
Lukas Drews <info@bytesandbits.net>
2022-02-24 15:17:26 +0100

    Add order view

--- src/store/models/Order.js
+++ src/store/models/Order.js
@@ -1,24 +1,24 @@
-/* eslint-disable import/no-cycle */
-import ApiBase from './ApiBase';
-import { User, OrderItem } from '.';
+import ApiBase from "./ApiBase";
+import { User, OrderItem } from ".";
 
 export default class Order extends ApiBase {
-  static entity = 'orders';
+  static entity = "orders";
 
-  static primaryKey = 'uuid';
+  static primaryKey = "uuid";
 
   static fields() {
     return {
       uuid: this.string(null),
       userUuid: this.string(null),
-      user: this.belongsTo(User, 'userUuid'),
-      items: this.hasMany(OrderItem, 'orderUuid'),
+      user: this.belongsTo(User, "userUuid"),
+      items: this.hasMany(OrderItem, "orderUuid"),
       amount: this.attr(null),
+      createdAt: this.attr(null),
     };
   }
 
   static apiConfig = {
     ...super.apiConfig,
-    url: '/orders',
+    url: "/orders",
   };
 }
---
+++ src/views/Orders.vue
@@ -0,0 +1,95 @@
+<template>
+  <section class="container mx-auto p-4 bg-white rounded">
+    <ConfirmDialog
+      v-model:active="confirmDialog"
+      @on-confirm="deleteItem(selected)"
+    />
+
+    <div class="flex justify-between items-center pb-4">
+      <h3>Orders</h3>
+      <o-button icon-right="redo" @click="getItems()" />
+    </div>
+
+    <o-table :data="data">
+      <o-table-column
+        v-slot="props"
+        field="user.fullName"
+        label="User"
+        sortable
+        >{{ props.row.user.fullName }}</o-table-column
+      >
+
+      <o-table-column v-slot="props" label="Items" sortable>
+        {{ getCount(props.row.items) }}
+      </o-table-column>
+
+      <o-table-column v-slot="props" field="amount" label="Amount" sortable>
+        {{ props.row.amount }}
+      </o-table-column>
+
+      <o-table-column v-slot="props" field="createdAt" label="Date" sortable>
+        {{ props.row.createdAt }}
+      </o-table-column>
+
+      <o-table-column v-slot="props" width="80">
+        <div class="float-right">
+          <o-icon
+            clickable
+            class="w-6 h-6"
+            icon="edit"
+            @click.stop="showForm(props.row)"
+          />
+          <o-icon
+            clickable
+            class="w-6 h-6"
+            icon="trash"
+            @click.stop="showConfirmDialog(props.row)"
+          />
+        </div>
+      </o-table-column>
+    </o-table>
+  </section>
+</template>
+
+<script>
+import apiClientMixin from "../mixins/apiClientMixin";
+import { Order } from "../store/models";
+import ConfirmDialog from "../components/dialogs/ConfirmDialog.vue";
+
+export default {
+  name: "OrderView",
+  components: {
+    ConfirmDialog,
+  },
+  mixins: [apiClientMixin],
+  data() {
+    return {
+      apiClient: Order,
+
+      selected: null,
+      confirmDialog: false,
+    };
+  },
+  computed: {
+    data() {
+      return Order.query().withAll().all();
+    },
+  },
+  created() {
+    this.getItems();
+  },
+  methods: {
+    getCount(items) {
+      return items.reduce((acc, curr) => acc + Number(curr.count), 0);
+    },
+    showForm(order) {
+      this.inputForm = true;
+      this.selected = order;
+    },
+    showConfirmDialog(order) {
+      this.confirmDialog = true;
+      this.selected = order;
+    },
+  },
+};
+</script>
