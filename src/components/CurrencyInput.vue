a85178eca83db286800f7b5247e4d89639cba372
Lukas Drews <info@bytesandbits.net>
2022-03-28 22:50:05 +0200

    Use composition api for CurrencyInput

--- src/components/CurrencyInput.vue
+++ src/components/CurrencyInput.vue
@@ -2,47 +2,39 @@
   <o-input ref="inputRef" type="text" required />
 </template>
 
-<script>
+<script setup>
 import { useCurrencyInput } from "vue-currency-input";
 import { watch } from "vue";
 
-export default {
-  name: "CurrencyInput",
-  props: {
-    modelValue: {
-      type: Number,
-      default: 0,
-    },
+const props = defineProps({
+  modelValue: {
+    type: Number,
+    default: 0,
   },
-  emits: ["update:modelValue"],
-  setup(props, { emit, refs }) {
-    const options = {
-      locale: "de-DE",
-      currency: "EUR",
-      currencyDisplay: "symbol",
-      precision: 2,
-      hideCurrencySymbolOnFocus: false,
-      hideGroupingSeparatorOnFocus: false,
-      hideNegligibleDecimalDigitsOnFocus: false,
-      autoDecimalDigits: true,
-      valueScaling: "precision",
-      autoSign: false,
-      useGrouping: false,
-      accountingSign: true,
-    };
+});
 
-    const { inputRef, setValue } = useCurrencyInput(options);
-
-    watch(
-      () => props.modelValue,
-      (value) => {
-        if (value === null) {
-          setValue(0);
-        }
-      }
-    );
-
-    return { inputRef };
-  },
+const options = {
+  locale: "de-DE",
+  currency: "EUR",
+  currencyDisplay: "symbol",
+  precision: 2,
+  hideCurrencySymbolOnFocus: false,
+  hideGroupingSeparatorOnFocus: false,
+  hideNegligibleDecimalDigitsOnFocus: false,
+  autoDecimalDigits: true,
+  valueScaling: "precision",
+  autoSign: false,
+  useGrouping: false,
+  accountingSign: false,
 };
+const { inputRef, setValue } = useCurrencyInput(options);
+
+watch(
+  () => props.modelValue,
+  (value) => {
+    if (value === null) {
+      setValue(0);
+    }
+  }
+);
 </script>
