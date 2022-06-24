62696b4c278a70aeb0ecb8e81a4a2e17b597d633
Lukas Drews <info@bytesandbits.net>
2022-06-24 18:37:46 +0200

    Display current groups in groups view

---
+++ src/views/Groups.vue
@@ -0,0 +1,34 @@
+<template>
+  <section class="container mx-auto p-4 bg-white rounded">
+    <o-table :data="data">
+      <template #empty>
+        <div class="m-4 text-center">No groups found</div> </template
+      >‚
+      <o-table-column v-slot="props" field="name" label="Group Name" sortable>
+        {{ props.row.name }}
+      </o-table-column>
+    </o-table>
+  </section>
+</template>
+
+<script>
+import apiClientMixin from "../mixins/apiClientMixin";
+
+import { Group } from "../store/models";
+export default {
+  name: "GroupView",
+  mixins: [apiClientMixin],
+  data() {
+    return {
+      apiClient: Group,
+    };
+  },
+  computed: {
+    data() {
+      return this.apiClient.query().withAll().all();
+    },
+  },
+};
+</script>
+
+<style scoped></style>
