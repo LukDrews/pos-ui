1e0a540ed47a649d459e0aee8c1725e6654f4b17
Lukas Drews <info@bytesandbits.net>
2022-06-24 19:57:38 +0200

    Use composition api for groups view

--- src/views/Groups.vue
+++ src/views/Groups.vue
@@ -1,34 +1,27 @@
 <template>
   <section class="container mx-auto p-4 bg-white rounded">
-    <o-table :data="data">
+    <o-table
+      :data="groups"
+    >
       <template #empty>
-        <div class="m-4 text-center">No groups found</div> </template
-      >‚
+        <div class="m-4 text-center">No groups found</div>
+      </template>
+
       <o-table-column v-slot="props" field="name" label="Group Name" sortable>
         {{ props.row.name }}
       </o-table-column>
     </o-table>
+
   </section>
 </template>
 
-<script>
-import apiClientMixin from "../mixins/apiClientMixin";
-
+<script setup>
+import { computed } from "vue";
 import { Group } from "../store/models";
-export default {
-  name: "GroupView",
-  mixins: [apiClientMixin],
-  data() {
-    return {
-      apiClient: Group,
-    };
-  },
-  computed: {
-    data() {
-      return this.apiClient.query().withAll().all();
-    },
-  },
-};
+// import { useApiClient } from "../composable/useApiClient";
+
+// const { getItems, addItem, updateItem, addOrUpdateItem, deleteItem } = useApiClient(Group);
+const groups = computed(() => Group.query().withAll().all());
 </script>
 
 <style scoped></style>
